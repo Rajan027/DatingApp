a2d0b2b7f3d540d26829543fdcbe9862f59cb83c
Rajan <rajanarora028@gmail.com>
2020-09-04 02:57:56 +0530

    filtering in app

--- DatingApp.API/Persistence/Repositories/DatingRepository.cs
+++ DatingApp.API/Persistence/Repositories/DatingRepository.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Core.Models;
 using DatingApp.API.Core.Repositories;
@@ -41,7 +43,20 @@ namespace DatingApp.API.Persistence.Repositories
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            return await PagedList<User>.CreateAsync(_context.Users.Include(p => p.Photos), userParams.PageNumber, userParams.PageSize);
+            var users = _context.Users.Include(p => p.Photos).AsQueryable();
+
+            users = users.Where(x => x.Id != userParams.UserId);
+
+            users = users.Where(x => x.Gender == userParams.Gender);
+
+            if(userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+                users = users.Where(x => x.DateOfBirth >= minDob && x.DateOfBirth <= maxDob);
+            }
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
